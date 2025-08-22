269b05b58e6690b593ddb8af9dd7ee1464563a70
nur-nayeem <nurnayem768@gmail.com>
2025-08-23 01:18:02 +0600

    added auth api route

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,50 @@
+import { connect } from '@/dbConfig/dbConfig'
+import User from '@/models/userModel'
+import bcryptjs from 'bcryptjs'
+import { sendMail } from '@/helpers/mailer'
+
+import { NextRequest, NextResponse } from 'next/server'
+
+connect()
+
+
+export async function POST(request: NextRequest) {
+    try {
+
+        const reqBody = await request.json()
+        const { username, email, password } = reqBody;
+
+        const user = await User.findOne({ email })
+        if (user) {
+            return NextResponse.json({ error: "user already exist" }, { status: 400 })
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+        console.log(typeof hashedPassword)
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+        const savedUser = await newUser.save()
+        console.log(savedUser)
+
+        //send verification email:
+
+        // await sendMail({ email, emailType: "VERIFY", userId: savedUser._id })
+
+        return NextResponse.json({
+            message: "user registraterd succecfully",
+            success: true,
+            savedUser
+        })
+
+    } catch (error: any) {
+        return NextResponse.json(
+            { error: error.message },
+            { status: 500 }
+
+        )
+    }
+}
