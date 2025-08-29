56196718e6eab123bdb6563ade81b8234792bfe2
nur-nayeem <nurnayem768@gmail.com>
2025-08-29 17:57:22 +0600

    added menu page

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,11 @@
+import FoodMenu from "@/components/FoodMenu";
+
+export default function Menu() {
+    return (
+        <div>
+            <section className="container mx-auto px-2.5 my-12">
+                <FoodMenu menuTitle='Menu' />
+            </section>
+        </div>
+    )
+}
